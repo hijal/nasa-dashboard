48624a074c6b5389049575a936eaab4d4e747d01
imh1j4l <hijal@shuttlebd.com>
2021-09-18 12:59:32 +0600

    api testing

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,62 @@
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Test GET /launches', () => {
+  test('It should respond with 201 status code', async () => {
+    await request(app)
+      .get('/launches')
+      .expect('Content-Type', /json/)
+      .expect(200);
+  });
+});
+
+describe('Test POST /launches', () => {
+  const completedLaunchData = {
+    mission: 'abc bd',
+    rocket: 'abc-rocket bd',
+    target: 'abc bd',
+    launchDate: 'June 1, 2029',
+  };
+  const launchDataWithoutDate = {
+    mission: 'abc bd',
+    rocket: 'abc-rocket bd',
+    target: 'abc bd',
+  };
+
+  const invalidLaunchDate = {
+    mission: 'abc bd',
+    rocket: 'abc-rocket bd',
+    target: 'abc bd',
+    launchDate: 'jun',
+  };
+
+  test('It should respond with 200 status code', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(completedLaunchData)
+      .expect('Content-Type', /json/)
+      .expect(201);
+
+    const reqLaunchDate = new Date(completedLaunchData.launchDate).valueOf();
+    const resLaunchDate = new Date(response.body.launchDate).valueOf();
+
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+    expect(resLaunchDate).toBe(reqLaunchDate);
+  });
+  test('It should catch missing required fields', async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(launchDataWithoutDate)
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(response.body).toStrictEqual({ error: 'Missing required fields.' });
+  });
+  test('It should catch invalid dates', async() => {
+    const response = await request(app)
+      .post('/launches')
+      .send(invalidLaunchDate)
+      .expect('Content-Type', /json/)
+      .expect(400);
+    expect(response.body).toStrictEqual({ error: 'Invalid launch date.' });
+  });
+});
